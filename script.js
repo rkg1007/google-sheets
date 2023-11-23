106f468c3a0ae95d4cc2e5d4238b3286ca47d483
rohit <rkg10072000@gmail.com>
2023-11-23 10:12:17 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,53 +1,75 @@
 const numOfRows = 101;
 const numOfCols = 27;
 
-const gridContainer = document.querySelector('.grid-container');
+const gridContainer = document.querySelector(".grid-container");
+const addressBar = document.querySelector(".address-bar");
+
+const addAddressBarListener = (cell, i, j) => {
+  cell.addEventListener("click", () => {
+    const rowAddress = i;
+    const colAddress = String.fromCharCode(65 + j - 1);
+    addressBar.value = `${colAddress}${rowAddress}`;
+  });
+};
 
 const createRowContainer = () => {
-    const rowContainer = document.createElement('div');
-    rowContainer.setAttribute('class', 'row-container');
-    return rowContainer;
-}
+  const rowContainer = document.createElement("div");
+  rowContainer.setAttribute("class", "row-container");
+  return rowContainer;
+};
 
 const createTopLeftCell = () => {
-    const cell = document.createElement('div');
-    cell.setAttribute('class', 'cell row-address col-address');
-    return cell;
-}
-
-const createColAddressCell = () => {
   const cell = document.createElement("div");
-  cell.setAttribute("class", "col-address cell");
+  cell.setAttribute("class", "cell row-address col-address");
   return cell;
 };
 
-const createRowAddressCell = () => {
+const createColAddressCell = (i, j) => {
+  const cell = document.createElement("div");
+  cell.setAttribute("class", "cell col-address");
+  cell.innerText = String.fromCharCode(65 + j - 1);
+  return cell;
+};
+
+const createRowAddressCell = (i, j) => {
   const cell = document.createElement("div");
   cell.setAttribute("class", "cell row-address");
+  cell.innerHTML = i;
   return cell;
 };
 
-const createEditableCell = () => {
+const createEditableCell = (i, j) => {
   const cell = document.createElement("div");
   cell.setAttribute("class", "cell");
-  cell.setAttribute('contenteditable', true);
+  cell.setAttribute("contenteditable", true);
+  cell.setAttribute("rid", i);
+  cell.setAttribute("cid", j);
+  addAddressBarListener(cell, i, j);
   return cell;
 };
 
-for (let i = 0; i < numOfRows; i++) {
-  const rowContainer = createRowContainer();
-  for (let j = 0; j < numOfCols; j++) {
-    let cell = null;
-    if (i == 0 && j == 0) {
-      cell = createTopLeftCell();
-    } else if (i == 0) {
-      cell = createColAddressCell();
-    } else if (j == 0) {
-      cell = createRowAddressCell();
-    } else {
-      cell = createEditableCell();
+const createGrid = () => {
+  for (let i = 0; i < numOfRows; i++) {
+    const rowContainer = createRowContainer();
+    for (let j = 0; j < numOfCols; j++) {
+      let cell = null;
+      if (i == 0 && j == 0) {
+        cell = createTopLeftCell(i, j);
+      } else if (i == 0) {
+        cell = createColAddressCell(i, j);
+      } else if (j == 0) {
+        cell = createRowAddressCell(i, j);
+      } else {
+        cell = createEditableCell(i, j);
+      }
+      rowContainer.appendChild(cell);
     }
-    rowContainer.appendChild(cell);
+    gridContainer.appendChild(rowContainer);
   }
-  gridContainer.appendChild(rowContainer);
+};
+
+const main = () => {
+    createGrid();
 }
+
+main();
